36f71d400dc7b582f752f6aaae4c55c8a9147b93
MatthewJOwens <mjowens@mail.com>
2020-04-10 23:22:13 -0600

    Comments Front End Started

---
+++ client/app/Controllers/CommentsController.js
@@ -0,0 +1,41 @@
+import commentsService from "../Services/CommentsService.js";
+import store from "../store.js";
+
+//Private
+function _draw() {
+  let comments = store.State.comments;
+  console.log(comments);
+
+  let template = ''
+
+  comments.forEach(comment => template += comment.Template)
+  document.getElementById("comments").innerHTML = template
+}
+
+
+//Public
+export default class CommentsController {
+  constructor() {
+    store.subscribe("comments", _draw);
+    this.getComments()
+    console.log("Comment Controller created")
+
+  }
+
+  getComments() {
+    commentsService.getComments()
+  }
+  create(event, id) {
+    event.preventDefault()
+    let formData = event.target
+    let newCommentObject = {
+      body: formData.body
+    }
+    commentsService.create(newCommentObject, id)
+    formData.reset()
+
+    // @ts-ignore
+    $('#add-comment-modal').modal('toggle')
+
+  }
+}
---
+++ client/app/Services/CommentsService.js
@@ -0,0 +1,48 @@
+import Comment from "../Models/Comment.js"
+import store from "../store.js";
+
+// @ts-ignore
+let _api = axios.create({
+  baseURL: '//localhost:3001/api/comments',
+  timeout: 10000
+})
+class CommentsService {
+  getComments() {
+    _api.get('')
+      .then(res => {
+        let comments = res.data.data.map(rawCommentData => new Comment(rawCommentData))
+        store.commit('comments', comments)
+      })
+      .catch(err => console.error(err))
+  }
+
+  delete(id) {
+    _api.delete(id)
+      .then(res => {
+        console.log(res)
+        this.getComments()
+      })
+      .catch(err =>
+        console.error(err))
+  }
+
+  create(newCommentObject, id) {
+    console.log(newCommentObject)
+    _api.post('', newCommentObject)
+      .then(res => {
+        console.log(res.data.data);
+        let newComment = new Comment(res.data.data)
+        let comments = [newComment, ...store.State.comments]
+        store.commit('comments', comments)
+      })
+      .catch(err => console.error(err))
+
+  }
+  constructor() {
+    this.getComments()
+  }
+
+}
+
+const service = new CommentsService();
+export default service;
